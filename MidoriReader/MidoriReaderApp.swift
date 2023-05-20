ece40f0176d00cb07ce7ff5bfc3d5ef6e47485e3
Long Kim Hoang <longkimhoang@outlook.com>
2023-05-21 00:31:08 +0700

    Initial commit

---
+++ MidoriReader/MidoriReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidoriReaderApp.swift
+//  MidoriReader
+//
+//  Created by Kim Long on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MidoriReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
